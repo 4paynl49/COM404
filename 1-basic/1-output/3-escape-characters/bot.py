7dff25784df254de026545369347e2d4ecd0aa95
4paynl49 <4paynl49@solent.ac.uk>
2019-09-24 10:43:40 +0000

    Added code to read user input.

---
+++ 1-basic/1-output/3-escape-characters/bot.py
@@ -0,0 +1,8 @@
+# Display escape characters
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Display a back slash")
+print("\" Displaya double quote")
+print("\' Display a single quote")
+print()
+print("\"I am Beep!")
