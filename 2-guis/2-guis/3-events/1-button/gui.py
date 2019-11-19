7a339f64268acf89a2f9c029259edb9fd3045ee5
Loic Payn <loic.payn@worldprogramming.com>
2019-11-19 12:07:48 +0000

    Added code to display a messagebox on user click.

--- 2-guis/2-guis/3-events/1-button/gui.py
+++ 2-guis/2-guis/3-events/1-button/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
     
@@ -7,6 +8,7 @@ class Gui(Tk):
         
         # set window attributes
         self.title("Tickets")
+        self.configure(bg="#ccc", padx=10, pady=10)
         
         # add components
         self.__add_outer_frame()
@@ -24,30 +26,37 @@ class Gui(Tk):
     
         
     def __add_heading_label(self):
-        self.heading_label = Label()
+        self.heading_label = Label(self.outer_frame)
         self.heading_label.grid(row=0, column=0)
 
         self.heading_label.configure(font="Arial 18",text="Entrance Ticket")
         
         
     def __add_instruction_label(self):
-        self.instruction_label = Label()
+        self.instruction_label = Label(self.outer_frame)
         self.instruction_label.grid(row=1, column=0, sticky=W)
 
         self.instruction_label.configure(font="Arial 12",text="How many tickets are needed?")
         
     def __add_tickets_entry(self):
-        self.tickets_entry = Entry()
-        self.tickets_entry.grid(row=2, column=0)
+        self.tickets_entry = Entry(self.outer_frame)
+        self.tickets_entry.grid(row=2, column= 0)
+        self.tickets_entry.configure(width= 40)
         
     def __add_buy_button(self):
-        self.buy_button = Button()
-        self.buy_button.grid(row=3, column=0, columnspan=2)
+        self.buy_button = Button(self.outer_frame)
+        self.buy_button.grid(row=3, column=0)
 
         self.buy_button.configure(font="Arial 11",text="Buy")
         self.buy_button.bind("<ButtonRelease-1>", self.__buy_button_clicked)
 
     def __buy_button_clicked(self, event):
-        self.messagebox.showinfo("Purchased!", "You have purchased the tickets!")
-  
-        
+        boughtTicket= int(self.tickets_entry.get())
+        if boughtTicket==1: 
+             messagebox.showinfo("Purchased!", "You have purchased 1 tickets!")
+
+        elif boughtTicket > 1:
+            messagebox.showinfo("Purchased!","You have purchesed " + str(boughtTicket) + " tickets!")
+
+        else: 
+            messagebox.showinfo("Purchased!", "You have entered an invalid number of tickets!")
