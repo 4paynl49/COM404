f8d782beaeb9bf321a3452b37d3585dbec1e34e7
Loic Payn <loic.payn@worldprogramming.com>
2019-11-26 12:27:59 +0000

    end of session

--- 2-guis/2-guis/4-images/3-positioning/gui.py
+++ 2-guis/2-guis/4-images/3-positioning/gui.py
@@ -1,3 +1,4 @@
+# import from tkinter
 from tkinter import *
 from tkinter import messagebox
 
@@ -25,23 +26,28 @@ class Gui(Tk):
         self.add_bus_image()
         
       
-
+        # create a heading label
     def add_heading_label(self):
         self.heading_label = Label()
         self.heading_label.grid(row=0, column=0)
 
         self.heading_label.configure(font="Arial 16",text="Bus Journey")
 
+        # create a frame
     def add_map_frame(self):
         self.map_frame = Frame()
         self.map_frame.grid(row=1, column=0)
+
         self.map_frame.configure(width=400, height=400)
 
+        # place map image in frame
     def add_map_image(self):
         self.map_image_label = Label(self.map_frame)
         self.map_image_label.place(x=0, y=0)
-        self.map_image_label.configure(image=self.map_image)
 
+        self.map_image_label.configure(image=self.map_image)
+        
+        # place bus image in frame
     def add_bus_image(self):
         self.bus_image_label = Label(self.map_frame)
         self.bus_image_label.place(x=10, y=10)
