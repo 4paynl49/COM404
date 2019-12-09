71068a1c416033b7fd3ea7f4a8b977bd4389f47b
Loic Payn <loic.payn@worldprogramming.com>
2019-12-09 15:39:04 +0000

    Vaild image function working

--- AE2 - Review TCA 1/Task A1/part_a.py
+++ AE2 - Review TCA 1/Task A1/part_a.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import re
+#import re
 
 
 
@@ -15,7 +15,9 @@ class Gui(Tk):
         self.configure(bg="#ccc",height=200,width=400,
                                  padx=10, pady=10)
         # load resources
-        self.mail_image = PhotoImage(file="./Mail.gif")
+        self.default_image = PhotoImage(file="./mail.gif")
+        self.valid_image = PhotoImage(file="./mail_green.gif")
+        self.nonValid_image = PhotoImage(file="./mail_red.gif")
 
         # add components
         self.__add_outer_frame()
@@ -57,9 +59,25 @@ class Gui(Tk):
     def __add_email_entry(self):
         self.email_entry = Entry(self.outer_frame)
         self.email_entry.grid(row=2, column=0)
-        self.email_entry.configure(borderwidth=2, width=35)
-    
-    # Create a subscribe button
+        self.email_entry.configure(fg="#f00",borderwidth=2, width=35)
+
+        self.email_entry.bind("<KeyRelease>", self.__email_entry_input)
+
+    def __email_entry_input(self, event):
+        if len(self.email_entry.get()) == 0:
+            self.mail_image_label.configure(image=self.nonValid_image)
+        else:
+            self.mail_image_label.configure(image=self.valid_image)
+
+    # load defualt image
+    def __add_mail_image_label(self):
+         self.mail_image_label = Label(self.outer_frame)
+         self.mail_image_label.grid(row=2, column=0, stick=E,padx=10)
+         self.mail_image_label.configure(image=self.default_image,
+                                             height=30,
+                                             width=50)
+ 
+    # create a subscribe button
     def __add_subscribe_button(self):
         self.subscribe_button = Button(self.outer_frame)
         self.subscribe_button.grid(row=3, column=0, columnspan=2, sticky=N+E+S+W)
@@ -67,27 +85,21 @@ class Gui(Tk):
                                         text= "Subscribe")
 
         self.subscribe_button.bind("<ButtonRelease-1>", self.__subscribe_button_clicked)
+        
 
     def __subscribe_button_clicked(self, event):
-      def email():
         addressToVerify = self.email_entry.get()
         match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
 
         if match == None:
-            messagebox.showinfo("Newsletter", "Bad Syntax")
+            messagebox.showinfo("Newsletter", "Enter Valid email address please")
+            
         else: 
             messagebox.showinfo("Newsletter", "Subscribed!")
+            
 
 
-
-    # # load defualt image
-    def __add_mail_image_label(self):
-         self.mail_image_label = Label(self.outer_frame)
-         self.mail_image_label.grid(row=2, column=0, stick=E,padx=10)
-         self.mail_image_label.configure(image=self.mail_image,
-                                             height=30,
-                                             width=50)
-
+  
 
 
 
