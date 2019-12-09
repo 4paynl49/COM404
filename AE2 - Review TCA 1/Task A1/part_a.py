b7294b735a399a1f85e99688a53f1fb48cbc4bfe
Loic Payn <loic.payn@worldprogramming.com>
2019-12-09 16:25:10 +0000

    Task B - complete

--- AE2 - Review TCA 1/Task A1/part_a.py
+++ AE2 - Review TCA 1/Task A1/part_a.py
@@ -64,10 +64,11 @@ class Gui(Tk):
         self.email_entry.bind("<KeyRelease>", self.__email_entry_input)
 
     def __email_entry_input(self, event):
-        if len(self.email_entry.get()) == 0:
-            self.mail_image_label.configure(image=self.nonValid_image)
-        else:
+        #if len(self.email_entry.get()) == 0:
+        if self.validEmail():
             self.mail_image_label.configure(image=self.valid_image)
+        else:
+            self.mail_image_label.configure(image=self.nonValid_image)
 
     # load defualt image
     def __add_mail_image_label(self):
@@ -86,16 +87,20 @@ class Gui(Tk):
 
         self.subscribe_button.bind("<ButtonRelease-1>", self.__subscribe_button_clicked)
         
-
-    def __subscribe_button_clicked(self, event):
+    def validEmail(self):
         addressToVerify = self.email_entry.get()
         match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
-
         if match == None:
-            messagebox.showinfo("Newsletter", "Enter Valid email address please")
-            
-        else: 
+            return False
+        else:
+            return True
+
+    def __subscribe_button_clicked(self, event):
+        if self.validEmail():
             messagebox.showinfo("Newsletter", "Subscribed!")
+
+        else: 
+            messagebox.showinfo("Newsletter", "Enter Valid email address please")
             
 
 
