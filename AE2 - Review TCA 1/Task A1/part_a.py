d5e5940454e4c9f52acbf11b51220d647f6c8aa1
Loic Payn <loic.payn@worldprogramming.com>
2019-12-09 18:56:24 +0000

    started to add option menu

--- AE2 - Review TCA 1/Task A1/part_a.py
+++ AE2 - Review TCA 1/Task A1/part_a.py
@@ -25,6 +25,8 @@ class Gui(Tk):
         self.__add_instruction_lable()
         self.__add_email_lable()
         self.__add_email_entry()
+        self.__add_sub_type_lable()
+        #self.__add_sub_type_optionMenu()
         self.__add_subscribe_button()
         self.__add_mail_image_label()
 
@@ -41,6 +43,7 @@ class Gui(Tk):
         self.heading_lable.grid(row=0, column=0, columnspan=2)
         self.heading_lable.configure(font="Arial 16", pady=10,
                                     text="RECEIVE OUR NEWSLETTER")
+
     # create a instruction lable
     def __add_instruction_lable(self):
         self.instruction_lable = Label(self.outer_frame)
@@ -72,15 +75,37 @@ class Gui(Tk):
 
     # load defualt image
     def __add_mail_image_label(self):
-         self.mail_image_label = Label(self.outer_frame)
-         self.mail_image_label.grid(row=2, column=0, stick=E,padx=10)
-         self.mail_image_label.configure(image=self.default_image,
+        self.mail_image_label = Label(self.outer_frame)
+        self.mail_image_label.grid(row=2, column=0, stick=E,padx=10)
+        self.mail_image_label.configure(image=self.default_image,
                                              height=30,
                                              width=50)
- 
+
+    # load a subscribe type
+    def __add_sub_type_lable(self):
+        self.sub_type_lable = Label(self.outer_frame)
+        self.sub_type_lable.grid(row=3, column=0, padx=10, pady=10)
+        self.sub_type_lable.configure(font="Arial 10",padx=10, pady=10,
+                                      text="Type:")
+
+    # # create sub type drop down list 
+    # def __add_sub_type_optionMenu(self):
+    #     self.sub_type_optionMenu = OptionMenu(self.outer_frame,tkvar, *choices)
+    #     # self.sub_type_optionMenu.grid(row=3, column=0, padx=10, pady=10)
+    #     # self.sub_type_optionMenu(fg="#f00",borderwidth=2, width=35)
+    #     Label(self.outer_frame, text="Type").grid(row = 3, column = 0)
+    #     sub_type_optionMenu.grid(row = 3, column =0)
+
+    #     # Create a Tkinter variable
+    #     tkvar = StringVar(root)
+
+    #     # Dictionary with options
+    #     choices = { 'Weekly','Monthly','Yearly'}
+    #     tkvar.set('Weekly') # set the default option
+
     # create a subscribe button
     def __add_subscribe_button(self):
-        self.subscribe_button = Button(self.outer_frame)
+        self.subscribe_button = Button()
         self.subscribe_button.grid(row=3, column=0, columnspan=2, sticky=N+E+S+W)
         self.subscribe_button.configure(bg="#fee",font="Arial 10", 
                                         text= "Subscribe")
